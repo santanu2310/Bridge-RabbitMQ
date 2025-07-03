39b7fb5fddef12918ad42ac92a0dc89707161a0c
santanu2310 <dev.santanu23@gmail.com>
2025-07-03 20:25:38 +0530

    add tool-tip

---
+++ client/src/components/NavButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="w-auto h-full lg:w-full lg:h-auto group relative inline-flex items-center justify-center cursor-pointer"
+  >
+    <!-- Slot for the trigger element -->
+    <div
+      class="w-auto lg:w-full h-full lg:h-auto p-1 aspect-square flex items-center justify-center"
+    >
+      <slot></slot>
+    </div>
+
+    <!-- Tooltip -->
+    <div
+      class="absolute left-full -ml-1 hidden group-hover:block whitespace-nowrap bg-gray-950 text-gray-100 text-sm px-3 py-1.5 rounded shadow-lg z-50 before:absolute before:right-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-r-gray-950 before:border-l-transparent before:border-t-transparent before:border-b-transparent"
+    >
+      {{ props.text }}
+    </div>
+  </div>
+</template>
--- client/src/containers/Navigation.vue
+++ client/src/containers/Navigation.vue
@@ -1,63 +1,56 @@
 <script setup lang="ts">
-	import { useUserStore } from "@/stores/user";
-	import IconLogo from "@/components/icons/IconLogo.vue";
-	import IconMessages from "@/components/icons/IconMessages.vue";
-	import IconGroupes from "@/components/icons/IconGroupes.vue";
-	import IconFriends from "@/components/icons/IconFriends.vue";
-	import IconCall from "@/components/icons/IconCall.vue";
+import { useUserStore } from "@/stores/user";
+import IconLogo from "@/components/icons/IconLogo.vue";
+import IconMessages from "@/components/icons/IconMessages.vue";
+import IconGroupes from "@/components/icons/IconGroupes.vue";
+import IconFriends from "@/components/icons/IconFriends.vue";
+import IconCall from "@/components/icons/IconCall.vue";
+import NavButton from "@/components/NavButton.vue";
 
-	const userStore = useUserStore();
+const userStore = useUserStore();
 </script>
 
 <template>
-	<div
-		class="w-full h-full flex flex-row lg:flex-col justify-between bg-color-background-soft"
-	>
-		<div
-			class="w-3/4 lg:w-full lg:h-fit flex justify-around lg:flex-col items-end"
-		>
-			<div
-				class="w-full h-auto aspect-square hidden lg:flex items-center justify-center text-primary"
-			>
-				<IconLogo />
-			</div>
-			<div
-				class="w-auto lg:w-full h-full lg:h-auto p-1 aspect-square lg:mt-2 flex items-center justify-center cursor-pointer"
-				@click="$emit('switchContainer', 'chats')"
-			>
-				<IconMessages :size="35" />
-			</div>
-			<div
-				class="w-auto lg:w-full h-full lg:h-auto p-1 aspect-square flex items-center justify-center cursor-pointer"
-			>
-				<IconGroupes :size="35" />
-			</div>
-			<div
-				class="w-auto lg:w-full h-full lg:h-auto p-1 aspect-square flex items-center justify-center cursor-pointer"
-				@click="$emit('switchContainer', 'friends')"
-			>
-				<IconFriends :size="35" />
-			</div>
-			<div
-				class="w-auto lg:w-full h-full lg:h-auto p-1 aspect-square flex items-center justify-center cursor-pointer"
-				@click="$emit('switchContainer', 'calllogs')"
-			>
-				<IconCall :size="35" />
-			</div>
-		</div>
-		<div
-			class="w-1/4 lg:w-full h-full lg:h-auto aspect-square flex flex-col items-center"
-		>
-			<div
-				class="lg:w-1/2 h-1/2 lg:h-auto aspect-square mt-3 mb-4 overflow-hidden object-cover rounded-full cursor-pointer"
-				@click="$emit('switchContainer', 'profile')"
-			>
-				<img
-					:src="userStore.user.profilePicUrl as string"
-					alt=""
-					class="w-full h-full"
-				/>
-			</div>
-		</div>
-	</div>
+  <div
+    class="w-full h-full flex flex-row lg:flex-col justify-between bg-color-background-soft"
+  >
+    <div
+      class="w-3/4 lg:w-full lg:h-fit flex justify-around lg:flex-col items-end"
+    >
+      <div
+        class="w-full h-auto aspect-square hidden lg:flex items-center justify-center text-primary"
+      >
+        <IconLogo />
+      </div>
+
+      <NavButton
+        :text="'Conversations'"
+        @click="$emit('switchContainer', 'chats')"
+      >
+        <IconMessages :size="35" />
+      </NavButton>
+
+      <NavButton :text="'Groups'"><IconGroupes :size="35" /></NavButton>
+      <NavButton :text="'Friends'" @click="$emit('switchContainer', 'friends')">
+        <IconFriends :size="35" />
+      </NavButton>
+      <NavButton :text="'Calls'" @click="$emit('switchContainer', 'calllogs')">
+        <IconCall :size="35" />
+      </NavButton>
+    </div>
+    <div
+      class="w-1/4 lg:w-full h-full lg:h-auto aspect-square flex flex-col items-center"
+    >
+    <NavButton :text="'Profile'" @click="$emit('switchContainer', 'profile')">
+      <div class="w-auto lg:w-3/5 h-3/5 lg:h-auto aspect-square overflow-hidden rounded-full cursor-pointer">
+        <img
+          :src="userStore.user.profilePicUrl as string"
+          alt=""
+          class="w-full h-full object-cover"
+        />
+      </div>
+      
+    </NavButton>
+    </div>
+  </div>
 </template>
