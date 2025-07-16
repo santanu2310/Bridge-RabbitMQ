0a2ff1b826ac0b7ff0c027ce9796946563caeea3
santanu <s.m.23102004@gmail.com>
2025-07-16 20:30:12 +0530

    update the profile pic for null value and some indentation change

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -1,100 +1,100 @@
 <script setup lang="ts">
-	import { onMounted, ref } from "vue";
-	import type { User } from "@/types/User";
-	import type { Message } from "@/types/Message";
-	import { getInitials } from "@/utils/StringUtils";
-	import { useUserStore } from "@/stores/user";
-	import { useFriendStore } from "@/stores/friend";
-	import { formatDateDifference } from "@/utils/DateUtils";
+import { onMounted, ref } from "vue";
+import type { User } from "@/types/User";
+import type { Message } from "@/types/Message";
+import { getInitials } from "@/utils/StringUtils";
+import { getUserColor } from "@/utils/ProfileUtils";
+import { useUserStore } from "@/stores/user";
+import { useFriendStore } from "@/stores/friend";
+import { formatDateDifference } from "@/utils/DateUtils";
 
-	const userStore = useUserStore();
-	const friendStore = useFriendStore();
-	const isOnline = userStore.useOnlineStatusManager().isOnline;
+const userStore = useUserStore();
+const friendStore = useFriendStore();
+const isOnline = userStore.useOnlineStatusManager().isOnline;
 
-	// Define props for component
-	const props = defineProps<{
-		id: string;
-		userId: string;
-		lastMessageDate: string;
-		messages: Message[];
-	}>();
+// Define props for component
+const props = defineProps<{
+  id: string;
+  userId: string;
+  lastMessageDate: string;
+  messages: Message[];
+}>();
 
-	// reactive state to hold user value
-	const user = ref<User | null>(null);
+// reactive state to hold user value
+const user = ref<User | null>(null);
 
-	// Calculate unseen messages count on component initialization
-	const unseenMessagesCount = props.messages.filter(
-		(message) => message.status != "seen"
-	).length;
+// Calculate unseen messages count on component initialization
+const unseenMessagesCount = props.messages.filter(
+  (message) => message.status != "seen"
+).length;
 
-	// Fetch user data on component mount
-	onMounted(async () => {
-		user.value = friendStore.friends[props.userId];
-	});
+// Fetch user data on component mount
+onMounted(async () => {
+  user.value = friendStore.friends[props.userId];
+});
 
-	// Function to set current conversation in user store
-	async function setCurrentConversation() {
-		userStore.currentConversation = {
-			receiverId: props.userId,
-			convId: props.id,
-		};
-		userStore.isChatVisible = true;
-	}
+// Function to set current conversation in user store
+async function setCurrentConversation() {
+  userStore.currentConversation = {
+    receiverId: props.userId,
+    convId: props.id,
+  };
+  userStore.isChatVisible = true;
+}
 </script>
 
 <template>
-	<div
-		class="w-full h-auto flex items-center cursor-pointer"
-		@click="setCurrentConversation()"
-		v-if="user"
-	>
-		<div
-			class="w-auto h-7 aspect-square flex items-center justify-center relative"
-		>
-			<div
-				class="w-full h-full aspect-square overflow-hidden rounded-full"
-			>
-				<img
-					v-if="user?.profilePicUrl"
-					:src="user?.profilePicUrl"
-					alt=""
-					class="w-full h-full object-cover"
-				/>
-				<div
-					v-else
-					class="w-full h-full flex items-center justify-center bg-red-500"
-				>
-					<span class="w-fit h-fit block text-white text-xs">{{
-						getInitials(user.fullName || (user.userName as string))
-					}}</span>
-				</div>
-			</div>
-			<div
-				v-if="isOnline(props.userId)"
-				class="w-3 h-auto aspect-square rounded-full absolute bg-color-background"
-				style="padding: 2px; bottom: -2px; right: -2px"
-			>
-				<span
-					class="w-full h-full aspect-square block rounded-full bg-green-400"
-				></span>
-			</div>
-		</div>
-		<div class="h-full flex items-center justify-between flex-grow">
-			<div class="p-2 flex">
-				<span class="text-color-heading text-sm font-medium">{{
-					user.fullName || (user.userName as string)
-				}}</span>
-			</div>
-			<div class="flex flex-col justify-end items-center">
-				<span
-					class="text-xs text-color-heading font-semibold"
-					v-if="unseenMessagesCount > 0"
-					>{{ unseenMessagesCount }}</span
-				>
-				<span class="text-xs font-light">{{
-					formatDateDifference(props.lastMessageDate)
-				}}</span>
-			</div>
-		</div>
-	</div>
+  <div
+    class="w-full h-auto flex items-center cursor-pointer"
+    @click="setCurrentConversation()"
+    v-if="user"
+  >
+    <div
+      class="w-auto h-7 aspect-square flex items-center justify-center relative"
+    >
+      <div class="w-full h-full aspect-square overflow-hidden rounded-full">
+        <img
+          v-if="user?.profilePicUrl"
+          :src="user?.profilePicUrl"
+          alt=""
+          class="w-full h-full object-cover"
+        />
+        <div
+          v-else
+          class="w-full h-full flex items-center justify-center"
+          :style="{ background: getUserColor(user.fullName) }"
+        >
+          <span class="w-fit h-fit block text-white text-xs">{{
+            getInitials(user.fullName || (user.userName as string))
+          }}</span>
+        </div>
+      </div>
+      <div
+        v-if="isOnline(props.userId)"
+        class="w-3 h-auto aspect-square rounded-full absolute bg-color-background"
+        style="padding: 2px; bottom: -2px; right: -2px"
+      >
+        <span
+          class="w-full h-full aspect-square block rounded-full bg-green-400"
+        ></span>
+      </div>
+    </div>
+    <div class="h-full flex items-center justify-between flex-grow">
+      <div class="p-2 flex">
+        <span class="text-color-heading text-sm font-medium">{{
+          user.fullName || (user.userName as string)
+        }}</span>
+      </div>
+      <div class="flex flex-col justify-end items-center">
+        <span
+          class="text-xs text-color-heading font-semibold"
+          v-if="unseenMessagesCount > 0"
+          >{{ unseenMessagesCount }}</span
+        >
+        <span class="text-xs font-light">{{
+          formatDateDifference(props.lastMessageDate)
+        }}</span>
+      </div>
+    </div>
+  </div>
 </template>
